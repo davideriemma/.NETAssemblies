0eeffaec401ea0cd31ae9568fd37eeea61739e73
DAVIDE RIEMMA <da.riemma@studenti.unina.it>
2022-02-17 20:01:37 +0100

    Added License file

--- TestLibraryAssemblyEntryPoint/Class1.cs
+++ TestLibraryAssemblyEntryPoint/Class1.cs
@@ -1,12 +1,22 @@
 ﻿namespace TestLibraryAssemblyEntryPoint
 {
+    /// <summary>
+    /// Assembly Entry Point Class
+    /// </summary>
     public class AssemblyEntryPoint
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public AssemblyEntryPoint()
         {
             //do nothing
         }
 
+        /// <summary>
+        /// Dummy Method
+        /// <return>nothing</return>
+        /// </summary>
         public void Boo()
         {
             return;
